bb923a6137d7ad6ec70864a6a3875bc4c6dbf80e
AldoAbdn <alistairaldo@hotmail.co.uk>
2020-07-05 22:39:36 +0100

    Fade in/out credits

--- js/GameEngine/GameManager.js
+++ js/GameEngine/GameManager.js
@@ -1,15 +1,16 @@
 class GameManager {
     constructor(){
         this.StartGame = this.StartGame.bind(this);
-        this.Reset = this.Reset.bind(this);
+        this.AnimationEnd = this.AnimationEnd.bind(this);
         this.GameOver = this.GameOver.bind(this);
         this.title = document.getElementById("TitleScreen");
         this.musicBar = document.getElementById("MusicBar");
         this.gameCanvas = document.getElementById("GameCanvas");
         this.endCredits = document.getElementById("EndCredits");
-        this.endCredits.addEventListener("webkitAnimatinEnd", this.Reset, false);
-        this.endCredits.addEventListener("animationend", this.Reset, false);
+        this.endCredits.addEventListener("webkitAnimatinEnd", this.AnimationEnd, false);
+        this.endCredits.addEventListener("animationend", this.AnimationEnd, false);
         document.addEventListener("GameOver", this.GameOver, false);
+        this.creditCount = 0;
     }
     
     StartGame() {
@@ -27,9 +28,16 @@ class GameManager {
         this.musicBar.style.display = "none";
         this.endCredits.style.display = "block";
     }
+
+    AnimationEnd() {
+        this.creditCount += 1;
+        if(this.creditCount >= this.endCredits.children.length)
+            this.Reset();
+    }
     
     Reset(){
-        // this.game.musicManager.Stop();
+        this.creditCount = 0;
+        this.game.musicManager.Stop();
         this.game = null;
         this.endCredits.style.display = "none";
         this.gameCanvas.style.display = "none";
--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 var gameManager = new GameManager();
+
+function OnLoad(){
+    let endTitles = document.getElementById("EndCredits");
+    let delay = 0;
+    for(child of endTitles.children){
+        child.style.animationDelay = delay + "s";
+        delay += 5;
+    }
+}
